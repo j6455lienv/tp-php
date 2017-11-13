4de0e4c8dd3469e42e8da7bfb601dc167b2813c5
j6455lienv <venet.julien@gmail.com>
2017-11-13 18:51:38 +0100

    2017-11-13-lastModif

--- php/tp4.php
+++ php/tp4.php
@@ -1,15 +1,2 @@
-<?php
 
-$cnx = mysqli_connect('localhost','root','','formation_php');
-
-//var_dump($cnx);//commentaire
-
-if(isset($cnx)){
-    $req = mysqli_query($cnx, 'SELECT * FROM usert');
-    //var_dump($req);
-}
-
-foreach ($req as $key => $value) {
-        echo $value['login']. '<br>';
-    }
 
